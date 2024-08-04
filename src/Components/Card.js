bb12b2206a34061207cea69512709bd86e8604cb
Martin Uche <martinuche19@outlook.com>
2024-08-04 18:03:01 -0500

    added hook effect

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,47 +1,35 @@
-import React from 'react'
+import React from 'react';
 import { useSelector } from 'react-redux';
 import moment from 'moment';
 import { Link } from 'react-router-dom';
 
-const Card = ({data, trending, index, media_type}) => {
-    const imageURL = useSelector(state => state.movieData.imageURL)
+const Card = ({ data, trending, index, media_type }) => {
+    const imageURL = useSelector(state => state.movieData.imageURL);
+    const mediaType = data.media_type ?? media_type;
 
-    const mediaType = data.media_type ?? media_type
-  return (
-    <Link to={"/"+mediaType+"/"+data.id} className='w-full min-w-[230px] max-w-[230px] overflow-hidden block rounded relative hover:scale-105 transition-all'>
-        
-        {
-            data?.poster_path ? (
-                <img
-                    src={imageURL+data?.poster_path}
-                />
+    return (
+        <Link to={`/${mediaType}/${data.id}`} className='w-full min-w-[230px] max-w-[230px] overflow-hidden block rounded relative hover:scale-105 transition-all'>
+            {data?.poster_path ? (
+                <img src={`${imageURL}${data.poster_path}`} alt={data.title || data.name} />
             ) : (
-                <div className='bg-neutral-800 h-full w-full justify-center items-center'>
+                <div className='bg-neutral-800 h-full w-full flex justify-center items-center'>
                     No image found
                 </div>
-            )
-        }
-        
-
-        <div className='absolute top-3'>
-            {
-                trending && (
-                    <div className='py-1 px-4 bg-red-600 backdrop-blur-3xl'>
-                        #{index} Trending
-                    </div>
-                )
-            }
-        </div>
-        
-        <div className='absolute bottom-0 h-16 backdrop-blur-3xl w-full bg-black/60 p-2'>
-            <h2 className='text-ellipsis line-clamp-1 text-lg font-semibold'>{data?.title || data?.name}</h2>
-            <div className='text-sm text-neutral-400 flex justify-between items-center'>
-                <p>{moment(data.release_date).format("MMMM Do YYYY")}</p>
-                <p className='bg-red-600 px-1 text-xs'> Rating: {Number(data.vote_average).toFixed(1)}</p>
+            )}
+            {trending && (
+                <div className='absolute top-3 py-1 px-4 bg-red-600 backdrop-blur-3xl'>
+                    #{index} Trending
+                </div>
+            )}
+            <div className='absolute bottom-0 h-16 backdrop-blur-3xl w-full bg-black/60 p-2'>
+                <h2 className='text-ellipsis line-clamp-1 text-lg font-semibold'>{data?.title || data?.name}</h2>
+                <div className='text-sm text-neutral-400 flex justify-between items-center'>
+                    <p>{moment(data.release_date).format("MMMM Do YYYY")}</p>
+                    <p className='bg-red-600 px-1 text-xs'>Rating: {Number(data.vote_average).toFixed(1)}</p>
+                </div>
             </div>
-        </div>
-    </Link>
-  )
-}
+        </Link>
+    );
+};
 
 export default Card;
--- src/Components/HomeBanner.js
+++ src/Components/HomeBanner.js
@@ -1,94 +1,83 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { FiArrowLeftCircle, FiArrowRightCircle } from "react-icons/fi";
+import { FiArrowLeftCircle, FiArrowRightCircle } from 'react-icons/fi';
 import PlayVideo from './PlayVideo';
 
 const HomeBanner = () => {
-  const bannerData = useSelector(state => state.movieData.bannerData);
-  const imageURL = useSelector(state => state.movieData.imageURL);
-  const [currentImage, setCurrentImage] = useState(0);
-  const [playVideo, setPlayVideo] = useState(false);
-  const [playVideoId, setPlayVideoId] = useState('');
+    const bannerData = useSelector(state => state.movieData.bannerData);
+    const imageURL = useSelector(state => state.movieData.imageURL);
+    const [currentImage, setCurrentImage] = useState(0);
+    const [playVideo, setPlayVideo] = useState(false);
+    const [playVideoId, setPlayVideoId] = useState('');
 
-  const handleNext = () => {
-    if (currentImage < bannerData.length - 1) {
-      setCurrentImage((prev) => prev + 1);
-    }
-  };
-
-  const handlePrev = () => {
-    if (currentImage > 0) {
-      setCurrentImage((prev) => prev - 1);
-    }
-  };
-
-  const handlePlayVideo = (videoId) => {
-    setPlayVideoId(videoId);
-    setPlayVideo(true);
-  };
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (currentImage < bannerData.length - 1) {
-        handleNext();
-      } else {
-        setCurrentImage(0);
-      }
-    }, 5000);
-
-    return () => clearInterval(interval);
-  }, [bannerData, imageURL, currentImage]);
-
-  return (
-    <section className='w-full h-full'>
-      {playVideo && (
-        <PlayVideo videoId={playVideoId} close={() => setPlayVideo(false)} media_type={'explore'} />
-      )}
-      <div className='flex min-h-full max-h-[95vh] overflow-hidden'>
-        {
-          bannerData.map((data, index) => {
-            return (
-              <div key={data.id + "HomeBanner" + index} className='min-w-full min-h-[450px] lg:min-h-full overflow-hidden relative group transition-all' style={{ transform: `translateX(-${currentImage * 100}%)` }}>
-                <div className='w-full h-full'>
-                  <img
-                    src={imageURL + data.backdrop_path}
-                    className='h-full w-full object-cover'
-                  />
-                </div>
-
-                <div className='absolute top-0 w-full h-full hidden items-center  justify-between px-5 group-hover:lg:flex'>
-                  <button onClick={handlePrev} className='p-1 rounded-full  text-3xl z-10 text-white'>
-                    <FiArrowLeftCircle />
-                  </button>
-                  <button onClick={handleNext} className='p-2 rounded-full  text-3xl z-10 text-white '>
-                    <FiArrowRightCircle />
-                  </button>
-                </div>
-
-                <div className='absolute top-0 w-full h-full bg-gradient-to-t from-neutral-900 to-transparent'>
-                </div>
-
-                <div className='container mx-auto'>
-                  <div className='w-full absolute bottom-0 max-w-md px-5'>
-                    <h2 className='font-bold text-3xl lg:text-4xl text-white drop-shadow-2xl'>{data.title}</h2>
-                    <p className='text-ellipsis line-clamp-3 my-2'>{data.overview}</p>
-                    <div className='flex items-center gap-4'>
-                      <p>Ratings: {Number(data.vote_average).toFixed(1)}</p>
-                      <span>|</span>
-                      <p>Views: {Number(data.popularity).toFixed(0)}</p>
-                    </div>
-                    <button onClick={() => handlePlayVideo(data.id)} className='bg-red-600 px-4 py-2 text-white font-bold rounded mt-4 hover:bg-white hover:text-black transition-all hover:scale-105'>
-                      Play
-                    </button>
-                  </div>
-                </div>
-              </div>
-            )
-          })
+    const handleNext = () => {
+        if (currentImage < bannerData.length - 1) {
+            setCurrentImage(prev => prev + 1);
         }
-      </div>
-    </section>
-  )
-}
+    };
+
+    const handlePrev = () => {
+        if (currentImage > 0) {
+            setCurrentImage(prev => prev - 1);
+        }
+    };
+
+    const handlePlayVideo = (videoId) => {
+        setPlayVideoId(videoId);
+        setPlayVideo(true);
+    };
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (currentImage < bannerData.length - 1) {
+                handleNext();
+            } else {
+                setCurrentImage(0);
+            }
+        }, 5000);
+
+        return () => clearInterval(interval);
+    }, [bannerData, currentImage]);
+
+    return (
+        <section className='w-full h-full'>
+            {playVideo && (
+                <PlayVideo videoId={playVideoId} close={() => setPlayVideo(false)} media_type='explore' />
+            )}
+            <div className='flex min-h-full max-h-[95vh] overflow-hidden'>
+                {bannerData.map((data, index) => (
+                    <div key={`${data.id}HomeBanner${index}`} className='min-w-full min-h-[450px] lg:min-h-full overflow-hidden relative group transition-all' style={{ transform: `translateX(-${currentImage * 100}%)` }}>
+                        <div className='w-full h-full'>
+                            <img src={`${imageURL}${data.backdrop_path}`} className='h-full w-full object-cover' alt={data.title} />
+                        </div>
+                        <div className='absolute top-0 w-full h-full hidden items-center justify-between px-5 group-hover:lg:flex'>
+                            <button onClick={handlePrev} className='p-1 rounded-full text-3xl z-10 text-white'>
+                                <FiArrowLeftCircle />
+                            </button>
+                            <button onClick={handleNext} className='p-2 rounded-full text-3xl z-10 text-white'>
+                                <FiArrowRightCircle />
+                            </button>
+                        </div>
+                        <div className='absolute top-0 w-full h-full bg-gradient-to-t from-neutral-900 to-transparent'></div>
+                        <div className='container mx-auto'>
+                            <div className='w-full absolute bottom-0 max-w-md px-5'>
+                                <h2 className='font-bold text-3xl lg:text-4xl text-white drop-shadow-2xl'>{data.title}</h2>
+                                <p className='text-ellipsis line-clamp-3 my-2'>{data.overview}</p>
+                                <div className='flex items-center gap-4'>
+                                    <p>Ratings: {Number(data.vote_average).toFixed(1)}</p>
+                                    <span>|</span>
+                                    <p>Views: {Number(data.popularity).toFixed(0)}</p>
+                                </div>
+                                <button onClick={() => handlePlayVideo(data.id)} className='bg-red-600 px-4 py-2 text-white font-bold rounded mt-4 hover:bg-white hover:text-black transition-all hover:scale-105'>
+                                    Play
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
+};
 
 export default HomeBanner;
